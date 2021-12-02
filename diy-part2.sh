99cff1fdc355e859cd29d9ac14b0a2a4e9446539
Rainswr <i@o2oec.com>
2021-12-03 03:27:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,12 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+sed -i 's/192.168.1.1/10.10.10.11/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168/10.10/g' package/base-files/files/bin/config_generate
+sed -i 's/).1/).10/g' package/base-files/files/bin/config_generate
+
+# 修改 argon 为默认主题
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
