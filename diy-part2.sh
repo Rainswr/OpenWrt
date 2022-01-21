3c22588f05fdaec4ceef4580760645eb8370b48c
Rainswr <i@o2oec.com>
2022-01-22 04:42:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,4 +21,4 @@ sed -i 's/).1/).10/g' package/base-files/files/bin/config_generate
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-wget -O dl/linux-5.11.16.tar.xz https://github.com/Rainswr/i350/releases/download/v1.0.0/linux-5.11.16.tar.xz
+wget -O dl https://github.com/Rainswr/i350/releases/download/v1.0.0/linux-5.11.16.tar.xz
