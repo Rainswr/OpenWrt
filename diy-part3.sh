24e75cccb9b2c59bda8d44f7a89aef432e2d40e0
Rainswr <i@o2oec.com>
2022-01-23 01:44:57 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -15,6 +15,8 @@ linuxVersionAll=$(head -1 include/kernel-5.10)
 linuxVersion="5.10"$(echo ${linuxVersionAll#*=})
 #新建临时目录
 #mkdir tmp
+ls dll
+cat include/kernel-5.10
 #下载驱动文件
 wget -P dll https://github.com/Rainswr/i350/releases/download/v1.0.0/igb-5.8.5.tar.gz
 #解压驱动文件到临时目录
