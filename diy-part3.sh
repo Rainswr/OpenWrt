10651b422b6278bd372be691c297dd9146675036
Rainswr <i@o2oec.com>
2022-01-24 01:03:04 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -10,25 +10,33 @@
 # Description: OpenWrt DIY script part 3 (After Download package)
 #
 
+#igbVersion="5.5.2"
+igbVersion="5.8.5"
+
+majorVersion="5.4"
+#majorVersion="5.10"
+
 #获取内核版本
-linuxVersionAll=$(head -1 include/kernel-5.10)
-linuxVersion="5.10"$(echo ${linuxVersionAll#*=})
+linuxVersionAll=$(head -1 include/kernel-$majorVersion)
+linuxVersion=$majorVersion$(echo ${linuxVersionAll#*=})
 #新建临时目录
 #mkdir tmp
 ls dl
 #下载驱动文件
-wget -P dl https://github.com/Rainswr/i350/releases/download/v1.0.0/igb-5.5.2.tar.gz
+wget -P dl https://github.com/Rainswr/i350/releases/download/v1.0.0/igb-$igbVersion.tar.gz
 #解压驱动文件到临时目录
-tar -xzvf dl/igb-5.5.2.tar.gz -C tmp
+tar -xzvf dl/igb-$igbVersion.tar.gz -C tmp
 #解压内核文件到临时目录并进入临时目录删除原本驱动文件并返回原来目录
 tar -Jxf dl/linux-$linuxVersion.tar.xz -C tmp && cd tmp/linux-$linuxVersion/drivers/net/ethernet/intel/igb && rm -rf * && cd ../../../../../../../
 #移动新的驱动文件到内核
-mv -f tmp/igb-5.5.2/src/* tmp/linux-$linuxVersion/drivers/net/ethernet/intel/igb
+mv -f tmp/igb-$igbVersion/src/* tmp/linux-$linuxVersion/drivers/net/ethernet/intel/igb
 #删除驱动缓存和旧的内核文件
-rm -rf dl/linux-$linuxVersion.tar.xz && rm -rf tmp/igb-5.5.2
+rm -rf dl/linux-$linuxVersion.tar.xz && rm -rf tmp/igb-$igbVersion
 #再次进入临时目录并打包内核
 cd tmp && tar -Jcf ../dl/linux-$linuxVersion.tar.xz linux-$linuxVersion --remove-files && cd ..
 #获取新打包内核哈希并更新至文件
 linuxHashAll=$(sha256sum dl/linux-$linuxVersion.tar.xz)
 linuxHash=${linuxHashALL:0:65}
-sed -i 's/[a-zA-Z0-9]{65}/$linuxHash/g' include/kernel-5.10
+sed -i 's/[a-zA-Z0-9]{65}/$linuxHash/g' include/kernel-$majorVersion
+
+
