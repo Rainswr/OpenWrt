2b2cd019e45feb004cfe74ac8c7c112024b4b1b7
Rainswr <i@o2oec.com>
2022-01-24 10:31:40 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -10,4 +10,4 @@
 # Description: OpenWrt DIY script part 3 (After Download package)
 #
 
-wget -o target/linux/x86/patches-5.10/585-igb.5.8.5.patch https://github.com/Rainswr/i350/releases/download/v1.0.0/585-igb.5.8.5_2.patch
+wget -o target/linux/generic/pending-5.10/785-igb.5.8.5.patch https://github.com/Rainswr/i350/releases/download/v1.0.0/585-igb.5.8.5_2.patch
