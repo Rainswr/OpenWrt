cdb014902b3157fefce064d2806506cbc9da8da9
Rainswr <i@o2oec.com>
2022-01-26 04:06:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,3 +21,7 @@ sed -i 's/).1/).10/g' package/base-files/files/bin/config_generate
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+#igb-intel
+sed -i 's/@PCI_SUPPORT +kmod-i2c-core/@LINUX_5_15 @PCI_SUPPORT +kmod-i2c-core/g' package/kernel/linux/modules/netdevices.mk
+sed -i 's/kmod-igb/kmod-igb kmod-igb-intel/g' target/linux/x86/Makefile
